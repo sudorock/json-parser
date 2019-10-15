8805a0e9dc25fc0159d2d0b547efeb6c1fa1f6a1
Sunil KS <sunil@Sunils-MacBook-Pro.local>
2019-10-15 16:36:08 +0530

    modified object-vals

--- src/json_parser/core.clj
+++ src/json_parser/core.clj
@@ -9,13 +9,9 @@
 
 (defn check-digit? [ch] (if ch (and (<= (int ch) 57) (>= (int ch) 48)) nil))
 
-(defn null-parser [string] (if (starts-with? string "null") [nil (subs string 4)] nil))
+(defn null-parser [s] (if (starts-with? s "null") [nil (subs s 4)] nil))
 
-(defn boolean-parser [string]
-      (cond
-        (starts-with? string "true") [true (subs string 4)]
-        (starts-with? string "false") [false (subs string 5)]
-        :else nil))
+(defn boolean-parser [s] (cond (starts-with? s "true") [true (subs s 4)] (starts-with? s "false") [false (subs s 5)] :else nil))
 
 (defn number-after-e [prev string]
       (let [after-e (first string), sign-exists (or (= after-e \-) (= after-e \+)), sign (case after-e \- - \+ + +)]
@@ -82,56 +78,52 @@
             (throw-error)))))
 
 (defn get-object-vals [string]
-      (if (= (first s) \,)
+      (if (= (first string) \,)
         (throw-error)
-        (loop [remain s, key nil, value nil, result {}]
-          (let [fst (first remain) rst (trim (subs remain 1))]
-            (cond
-              (nil? fst) (throw-error)
-              (= fst \}) (let [result (if (or key value) (conj result (hash-map key value)) {})] (if (empty? rst) [result nil] [result rst]))
-              (= fst \:) (if-let [[val remaining] (gen-parser rst)]
-                           (recur (trim remaining) key val result)
-                           (throw-error))
-              (= fst \,) (if-let [[val remaining] (string-parser rst)]
-                           (recur (trim remaining) val nil (conj result (hash-map key value)))
-                           (throw-error))
-              :else (if-let [[val remaining] (string-parser remain)]
-                      (recur (trim remaining) val value result)
-                      (throw-error)))))))
+        (loop [[val remain] (gen-parser (trim string)), result []]
+          (if remain
+            (let [trimmed (trim remain) fst (first trimmed) rst (trim (subs trimmed 1)) res (conj result val)]
+              (cond
+                (= fst \}) (let [result (apply hash-map res)] (if (empty? rst) [result nil] [result rst]))
+                (or (= fst \:) (= fst \,)) (recur (gen-parser rst) res)
+                :else (throw-error)))
+            (throw-error)))))
 
-(defn array-parser [s]
-      (let [string (trim s)]
-        (cond
-          (= (first string) \[) (get-array-vals (subs string 1))
-          :else nil)))
+;(defn get-object-vals [string]
+;      (if (= (first string) \,)
+;        (throw-error)
+;        (loop [remain string, key nil, value nil, result {}]
+;          (let [fst (first remain) rst (trim (subs remain 1))]
+;            (cond
+;              (nil? fst) (throw-error)
+;              (= fst \}) (let [result (if (or key value) (conj result (hash-map key value)) {})] (if (empty? rst) [result nil] [result rst]))
+;              (= fst \:) (if-let [[val remaining] (gen-parser rst)]
+;                           (recur (trim remaining) key val result)
+;                           (throw-error))
+;              (= fst \,) (if-let [[val remaining] (string-parser rst)]
+;                           (recur (trim remaining) val nil (conj result (hash-map key value)))
+;                           (throw-error))
+;              :else (if-let [[val remaining] (string-parser remain)]
+;                      (recur (trim remaining) val value result)
+;                      (throw-error)))))))
 
-(defn object-parser [s]
-      (let [string (trim s)]
-        (cond
-          (= (first string) \{) (get-object-vals (subs string 1))
-          :else nil)))
 
-(defn gen-parser [string]
-      (let [null-parsed (null-parser string)
-            boolean-parsed (boolean-parser string)
-            string-parsed (string-parser string)
-            number-parsed (number-parser string)
-            array-parsed (array-parser string)
-            object-parsed (object-parser string)]
-        (or null-parsed boolean-parsed string-parsed number-parsed array-parsed object-parsed)))
+(defn array-parser [s] (let [string (trim s)] (if (= (first string) \[) (get-array-vals (subs string 1)) nil)))
 
-;(defn json-parser [s]
-;      (let [string (trim s) start (first string) end (get string (dec (count string)))]
-;        (cond
-;          (and (= start \[) (= end \])) (let [[result rst] (array-parser string)] (if rst (throw-error) result))
-;          (and (= start \{) (= end \})) (let [[result rst] (object-parser string)] (if rst (throw-error) result))
-;          :else (throw-error))))
+(defn object-parser [s] (let [string (trim s)] (if (= (first string) \{) (get-object-vals (subs string 1)) nil)))
+
+(defn gen-parser [s]
+      (let [null (null-parser s) bool (boolean-parser s) string (string-parser s)
+            number (number-parser s) array (array-parser s) obj (object-parser s)]
+        (or null bool string number array obj)))
 
 (defn json-parser [s]
       (let [string (trim s) [parsed remaining] (gen-parser string)]
-        (cond
-          (or (vector? parsed) (map? parsed)) (if remaining (throw-error) parsed)
-          :else (throw-error))))
+        (if (or (vector? parsed) (map? parsed))
+          (if remaining (throw-error) parsed)
+          (throw-error))))
+
+;; Testing ;;
 
 (def fail-cases
   (let [path "test/test_cases/fail@.json"]
