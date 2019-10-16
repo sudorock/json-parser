31242e1c76e6e50566a589604e36fece9a1f6672
Sunil KS <sunil@Sunils-MacBook-Pro.local>
2019-10-16 10:53:12 +0530

    added resultify

--- src/json_parser/core.clj
+++ src/json_parser/core.clj
@@ -7,11 +7,15 @@
 
 (defn throw-error [] "Parse Error")
 
+(def esc-char {\\ \\, \t \tab, \n \newline, \f \formfeed, \b \backspace, \r \return, \" \", \/ \/, \u \u})
+
 (defn check-digit? [ch] (if ch (and (<= (int ch) 57) (>= (int ch) 48)) nil))
 
 (defn null-parser [s] (if (starts-with? s "null") [nil (subs s 4)] nil))
 
 (defn boolean-parser [s] (condp #(starts-with? %2 %1) s "true" [true (subs s 4)] "false" [false (subs s 5)] nil))
+
+(defn resultify [result remaining] (if (empty? remaining) [result nil] [result remaining]))
 
 (defn number-after-e [prev string]
       (let [after-e (first string), sign-exists (or (= after-e \-) (= after-e \+)), sign (case after-e \- - \+ + +)]
@@ -28,7 +32,7 @@
       (loop [s string, result "0."]
         (let [fst (first s)]
          (cond
-           (nil? fst) (let [res (+ prev (Double/parseDouble result))] (if (empty? s) [res nil] [res s]))
+           (nil? fst) (resultify (+ prev (Double/parseDouble result)) s)
            (check-digit? fst) (recur (subs s 1) (str result fst))
            (or (= fst \e) (= fst \E)) (number-after-e (+ prev (Double/parseDouble result)) (subs s 1))
            :else [(+ prev (Double/parseDouble result)) s]))))
@@ -37,7 +41,7 @@
       (loop [s string result ""]
         (let [fst (first s)]
           (cond
-            (nil? fst) (if (empty? s) [(Long/parseLong result) nil] [(Long/parseLong result) s])
+            (nil? fst) (resultify (Long/parseLong result) s)
             (check-digit? fst) (recur (subs s 1) (str result fst))
             (= fst \.) (number-after-point (Double/parseDouble result) (subs s 1))
             (or (= fst \e) (= fst \E)) (number-after-e (Double/parseDouble result) (subs s 1))
@@ -49,20 +53,13 @@
          (= fst \0) (cond
                       (check-digit? snd) nil
                       (= snd \.) (if (check-digit? third) (get-number string) nil)
-                      :else (let [remain (subs string 1)] (if (empty? remain) [0 nil] [0 remain])))
-         (check-digit? fst) (get-number string)
+                      :else (resultify 0 (subs string 1)))
          (= fst \+) (get-number (subs string 1))
          (= fst \-) (update (get-number (subs string 1)) 0 #(- %))
+         (check-digit? fst) (get-number string)
          :else nil)))
 
-(def esc-char {\\ \\, \t \tab, \n \newline, \f \formfeed, \b \backspace, \r \return, \" \", \/ \/, \u \u})
-
-(defn get-esc [rst]
-      (let [esc (get esc-char (second rst))]
-        (if (some? esc) esc false)))
-
-(defn get-hex [s]
-      (read-string s))
+(defn get-esc [rst] (let [esc (get esc-char (second rst))] (if (some? esc) esc false)))
 
 (defn string-parser [string]
       (if ((complement starts-with?) string "\"")
@@ -70,34 +67,30 @@
         (loop [rst (subs string 1) result ""]
           (let [fst (first rst) esc (if (= fst \\) (get-esc rst) nil)]
            (cond
-             (and (= fst \\) (= (second rst) \u)) (recur (subs rst 6) (str result (get-hex (subs rst 0 6))))
+             (and (= fst \\) (= (second rst) \u)) (recur (subs rst 6) (str result (read-string (subs rst 0 6))))
              (some? esc) (if (false? esc) nil (recur (subs rst 2) (str result esc)))
              (or (= fst \tab) (= fst \newline)) nil
-             (= fst \") (let [remain (subs rst 1)] (if (empty? remain) [result nil] [result remain]))
+             (= fst \") (resultify result (subs rst 1))
              :else (recur (subs rst 1) (str result fst)))))))
 
 (defn get-array-vals [string]
-      (condp = (first string)
-        \, (throw-error)
-        \] (let [rst (trim (subs string 1))] (if (empty? rst) [[] nil] [[] rst]))
+      (condp = (first string) \, (throw-error) \] (resultify [] (trim (subs string 1)))
         (loop [[val remain] (gen-parser (trim string)), result []]
           (if remain
             (let [trimmed (trim remain) fst (first trimmed) rst (trim (subs trimmed 1)) res (conj result val)]
               (condp = fst
-                \] (if (empty? rst) [res nil] [res rst])
+                \] (resultify res rst)
                 \, (recur (gen-parser rst) res)
                (throw-error)))
             (throw-error)))))
 
 (defn get-object-vals [string]
-      (condp = (first string)
-        \, (throw-error)
-        \} (let [rst (trim (subs string 1))] (if (empty? rst) [{} nil] [{} rst]))
+      (condp = (first string) \, (throw-error) \} (resultify {} (trim (subs string 1)))
         (loop [[val remain] (gen-parser (trim string)), result []]
           (if remain
             (let [trimmed (trim remain) fst (first trimmed) rst (trim (subs trimmed 1)) res (conj result val)]
               (condp = fst
-                \} (let [result (apply hash-map res)] (if (empty? rst) [result nil] [result rst]))
+                \} (resultify (apply hash-map res) rst)
                 \: (recur (gen-parser rst) res)
                 \, (recur (string-parser rst) res)
                 (throw-error)))
